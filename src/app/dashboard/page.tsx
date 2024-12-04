03780004ea552ab725e77bdb5ec9b8e0e784d60c
Jatin Pant <kineticenergy96@outlook.com>
2024-12-04 05:34:49 +0530

    completed 13 vids

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+function BarChart() {
+  return <h1>Bar Chart</h1>;
+}
+
+export default function Dashboard() {
+  return (
+    <>
+      <h1>Dashboard</h1>
+      <BarChart />
+    </>
+  );
+}
---
+++ src/app/products/page.tsx
@@ -0,0 +1,10 @@
+export default function ProductList() {
+  return (
+    <>
+      <h1>Product List</h1>
+      <div>Product 1</div>
+      <div>Product 2</div>
+      <div>Product 3</div>
+    </>
+  );
+}
